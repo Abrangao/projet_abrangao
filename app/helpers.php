89d9fcf76ebbc2ba8b33d5c86dd48c382c388dd6
Abrangao <abrangao98@gmail.com>
2019-09-17 10:39:11 +0000

    ajout de fonction set_active_route dans helpers

--- app/helpers.php
+++ app/helpers.php
@@ -9,3 +9,8 @@ if(! function_exists('page_title')){
         }
     }
 }
+if(! function_exists('set_active_route')){
+    function set_active_route($route){
+        return Route::is($route) ? 'active' : '';
+    }
+}
